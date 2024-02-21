2cee8f045b393650c2ca53fa06232fde364ea482
quan <huynhkiengquan@gmail.com>
2024-02-21 17:36:57 +0700

    Update new client

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,8 +1,12 @@
 import { Routes } from '@angular/router';
 import { DefaulComponent } from './shared/layouts/defaul/defaul.component';
 import { LoginComponent } from './pages/login/login.component';
+import { NotFoundComponent } from './pages/not-found/not-found.component';
+import { AboutComponent } from './pages/about/about.component';
 
 export const routes: Routes = [
-    {path:'login',component:DefaulComponent,children:[{path:'',component:LoginComponent}]}
+    {path:'login',component:DefaulComponent,children:[{path:'',component:LoginComponent}]},
+    {path:"about",component:AboutComponent},
+    {path:"**",component:NotFoundComponent}
 ];
 
