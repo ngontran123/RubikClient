075f65975689f310aae84c0610cca9c0c2c2a9f6
quân huỳnh <huynhkiengquan@gmail.com>
2024-03-07 00:17:23 +0700

    Update rotating

--- src/app/pages/rubik-solve/rubik-solve.component.ts
+++ src/app/pages/rubik-solve/rubik-solve.component.ts
@@ -18,6 +18,11 @@ export class RubikSolveComponent {
   startRotateX:number=0;
   startRotateY:number=0;
   cubeRotateStyle:string='';
+  rotationX:number=0;
+  rotationY:number=0;
+  curr_horizontal_idx:number=0;
+  color_face:string[]=['green','red','blue','orange'];
+  is_upside_down:boolean=false;
 showValue()
 {
   alert("clickable");
@@ -52,14 +57,78 @@ stopRotate()
 rotateCube(event:MouseEvent)
 {
   if(this.isRotating)
-  {  
-    const deltaX=event.clientX-this.startPosX;
-    const deltaY=event.clientY-this.startPostY;
-    const rotateX=this.startRotateX+(deltaY/5);
-    const rotateY=this.startPostY+(deltaX/5);
-    this.startRotateX=this.startPosX;
-    this.startRotateY=this.startPostY;
-    this.cubeRotateStyle=`rotateX(${rotateX}deg) rotateY(${rotateY}deg)`;
+  { 
+    // const deltaX=event.clientX-this.startPosX;
+    // const deltaY=event.clientY-this.startPostY;
+    // const rotateX=this.startRotateX+(deltaY/5);
+    // const rotateY=this.startPostY+(deltaX/5);
+    // this.startRotateX=this.startPosX;
+    // this.startRotateY=this.startPostY;
+    this.startRotateX=event.clientX;
+    this.startRotateY=event.clientY;
+    const deltaX=this.startRotateX-this.startPosX;
+    const deltaY=this.startRotateY-this.startPostY;
+    this.rotationX+=deltaY*0.8;
+    this.rotationY+=deltaX*0.8;
+    this.startPosX=this.startRotateX;
+    this.startPostY=this.startRotateY;
+    console.log('X'+this.rotationX);
+    console.log('Y'+this.rotationY);
+    this.cubeRotateStyle=`rotateX(${this.rotationX}deg) rotateY(${this.rotationY}deg)`;
+  }
+}
+
+rotateRightButton()
+{
+  this.curr_horizontal_idx-=1;
+  if(this.curr_horizontal_idx<0)
+  {
+    this.curr_horizontal_idx=3;
+  }
+ if(this.is_upside_down)
+ {
+  switch(this.curr_horizontal_idx)
+{
+  case 0:this.cubeRotateStyle=`rotateX(-32deg) rotateY(320deg)`;break;
+  case 1: this.cubeRotateStyle=`rotateX(-32deg) rotateY(225deg)`;break;
+  case 2: this.cubeRotateStyle=`rotateX(-32deg) rotateY(130deg)`;break;
+  case 3: this.cubeRotateStyle=`rotateX(-32deg) rotateY(45deg)`;break;
+}
+ }
+ else
+ {
+
+ }
+}
+
+rotateLeftButton()
+{
+this.curr_horizontal_idx+=1;
+if(this.curr_horizontal_idx>3)
+{
+  this.curr_horizontal_idx=0;
+}
+if(this.is_upside_down)
+{
+switch(this.curr_horizontal_idx)
+{
+  case 0:this.cubeRotateStyle=`rotateX(-32deg) rotateY(320deg)`;break;
+  case 1: this.cubeRotateStyle=`rotateX(-32deg) rotateY(225deg)`;break;
+  case 2: this.cubeRotateStyle=`rotateX(-32deg) rotateY(130deg)`;break;
+  case 3: this.cubeRotateStyle=`rotateX(-32deg) rotateY(45deg)`;break;
+}
+}
+else
+{
+
+}
+}
+
+rotateUpsideDown()
+{  this.is_upside_down=!this.is_upside_down
+  switch(this.curr_horizontal_idx)
+  {
+
   }
 }
 
